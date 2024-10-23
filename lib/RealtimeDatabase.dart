e2e69a307dada6df9fe3887b29b64772aa5e3ee4
Md. Jakaria Ibna Azam Khan. <mdjakariaibnaazamkhan@gmail.com>
2024-10-23 21:15:45 +0600

    get lot time emulator error but emulator is well. Be careful main.dart file when connect firebase. Don't miss options.

--- lib/RealtimeDatabase.dart
+++ lib/RealtimeDatabase.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class Realtimedatabase extends StatefulWidget {
@@ -8,10 +9,68 @@ class Realtimedatabase extends StatefulWidget {
 }
 
 class _RealtimedatabaseState extends State<Realtimedatabase> {
+  late DatabaseReference _dbref;
+  String databaseJson = "";
+  int countvalue = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // when app open automatically get data from database
+    connectRealTimeDatabase();
+  }
+  void connectRealTimeDatabase(){
+  _dbref = FirebaseDatabase.instance.ref();  // at first get instance
+
+    _dbref.child('myCountKey').child('key_counter').onValue.listen((event){
+      print('Counter update: '+ event.snapshot.value.toString());
+      setState(() {
+        // countvalue int tai String ke int a convert korsi
+        countvalue = int.parse(event.snapshot.value.toString());
+      });
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: SafeArea(child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(padding: EdgeInsets.all(8),
+            child: Text(countvalue.toString()+ " database - "+databaseJson),
+            ),
+
+            TextButton(onPressed: (){
+
+            }, child: Text('Create DB')),
+            TextButton(onPressed: (){
+
+            }, child: Text('Read value')),
+            TextButton(onPressed: (){
+
+            }, child: Text('Read Once Child')),
+            TextButton(onPressed: (){
+
+            }, child: Text('Update Value')),
+            TextButton(onPressed: (){
+
+            }, child: Text('Update Counter value by 1')),
+            TextButton(onPressed: (){
+
+            }, child: Text('Delete Value'))
+          ],
+        ),
+      )),
 
     );
+  }
+  _createDB(){
+    // 2 ta row(document) create hobe profile and jobprofile
+    _dbref.child('profile').set("Jakaria Profile");
+    _dbref.child('jobprofile').set({  // add data in json format(map<String, dynamic>)
+      "website1": "www.jakaria.com",
+      "website2": "www.khan.com"
+    });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_realdatabase/RealtimeDatabase.dart';
+import 'package:firebase_realdatabase/firebase_options.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    // if you miss this get emulator error but you use new emulator problem not solved
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
@@ -12,6 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
