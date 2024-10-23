2c2e5f849df118ce8f35e160316edeb3b70f2132
Md. Jakaria Ibna Azam Khan. <mdjakariaibnaazamkhan@gmail.com>
2024-10-23 19:13:46 +0600

    added firebase dependency without tell version name. This is very useful because flutter auytomatically get updated version dependency.

---
+++ android/app/src/main/kotlin/com/example/firebase_realdatabase/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.firebase_realdatabase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/RealtimeDatabase.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Realtimedatabase extends StatefulWidget {
+  const Realtimedatabase({super.key});
+
+  @override
+  State<Realtimedatabase> createState() => _RealtimedatabaseState();
+}
+
+class _RealtimedatabaseState extends State<Realtimedatabase> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
